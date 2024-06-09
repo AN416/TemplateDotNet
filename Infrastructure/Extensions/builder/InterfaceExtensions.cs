5df03378c4af8334df07b7927293a176ae5ca2cb
Ayman Sulaiman alhazmi <hazmiasa@rcjy.gov.sa>
2024-06-09 15:01:15 +0300

    Add project files.

---
+++ Infrastructure/Extensions/builder/InterfaceExtensions.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Extensions.builder
+{
+    public static class InterfaceExtensions
+    {
+        public static IServiceCollection Interfaces(this IServiceCollection services, IConfiguration config)
+        {
+            
+            return services;
+        }
+
+
+    }
+}
---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
